4dda566a0d21b838e1fb11cd47de06399fab18ad
amigcamel <amigcamel@gmail.com>
2020-10-30 00:12:31 +0800

    make paths of service-worker.js and manifest.json relative

--- static/index.js
+++ static/index.js
@@ -40,7 +40,7 @@ window.onerror = function(message, source, lineno, colno, error) {
 }
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('service-worker.js');
   });
 }
 
