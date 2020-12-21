46dd066695028bf58da8dafbc79ad5e4e4c26c30
amigcamel <amigcamel@gmail.com>
2020-12-21 10:52:58 +0800

    add protrait placeholder

--- static/textboxbuttons.vue
+++ static/textboxbuttons.vue
@@ -53,7 +53,8 @@ limitations under the License.
   };
 
   function genSidInfo(sid) {
-    return `<img src="https://lh3.googleusercontent.com/a-/${HSDIC[sid].uri}" style="font-size: 1rem; font-style: inherit; height: 25px"> ${sid.slice(1,4)} ${HSDIC[sid].name.split(' ')[0]}`;
+    const imgUrl = HSDIC[sid].uri ? `https://lh3.googleusercontent.com/a-/${HSDIC[sid].uri}` : 'https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png';
+    return `<img src="${imgUrl}" style="font-size: 1rem; font-style: inherit; height: 25px"> ${sid.slice(1,4)} ${HSDIC[sid].name.split(' ')[0]}`;
   }
 
   export default {
